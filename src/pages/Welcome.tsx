03a2fc389876ce4a875b7167d07d1a9453e6cdbf
Giovaniavs <gavs2@cin.ufpe.br>
2021-04-19 11:52:30 -0300

    Feature(components): created a component folder and a button component

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import { SafeAreaView, Text, Image, TouchableOpacity, StyleSheet } from 'react-native';
+import { SafeAreaView, Text, Image, StyleSheet } from 'react-native';
 
 import wateringImg from '../assets/watering.png';
 import colors from '../styles/colors';
+import { Button } from '../components/Button'
 
 export function Welcome() {
   return (
@@ -12,20 +13,13 @@ export function Welcome() {
         suas plantas {'\n'}
         de forma fácil
       </Text>
-
       <Image source={wateringImg} style={styles.image}/>
-
       <Text style={styles.subtitle}>
         Não esqueça mais de regar suas
         plantas. Nós cuidamos de lembrar
         você sempre que precisar.
       </Text>
-
-      <TouchableOpacity style={styles.button} activeOpacity={0.7}>
-        <Text style={styles.buttonText}>
-          {'>'}
-        </Text>
-      </TouchableOpacity>
+      <Button />
     </SafeAreaView>
   )
 }
@@ -49,21 +43,8 @@ const styles = StyleSheet.create({
     paddingHorizontal: 20,
     color: colors.heading
   },
-  button: {
-    backgroundColor: colors.green,
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderRadius: 16,
-    marginBottom: 10,
-    height: 56,
-    width: 56
-  },
   image: {
     width: 292,
     height: 284
-  },
-  buttonText: {
-    color: colors.white,
-    fontSize: 24
   }
 })
