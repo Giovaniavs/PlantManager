5551d232bf5e18c6710c10ac201a143070363b8c
Giovaniavs <gavs2@cin.ufpe.br>
2021-04-20 08:48:05 -0300

    Hotfix(resizing): resized the Welcome screen for fit better in another screens

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,36 +1,39 @@
 import React from 'react';
 import {
-  SafeAreaView,
+  View,
   Text,
   Image,
   TouchableOpacity,
   Dimensions,
   StyleSheet
 } from 'react-native';
+import { Feather } from '@expo/vector-icons'
 
 import wateringImg from '../assets/watering.png';
 import colors from '../styles/colors';
+import fonts from '../styles/fonts'
 
 export function Welcome() {
   return (
-    <SafeAreaView style={styles.container}>
+    <View style={styles.container}>
       <Text style={styles.title}>
         Gerencie {'\n'} 
-        suas plantas {'\n'}
-        de forma fácil
+        suas plantas de {'\n'}
+        forma fácil
       </Text>
+
       <Image source={wateringImg} style={styles.image} resizeMode="contain"/>
+      
       <Text style={styles.subtitle}>
         Não esqueça mais de regar suas
         plantas. Nós cuidamos de lembrar
         você sempre que precisar.
       </Text>
+
       <TouchableOpacity style={styles.button} activeOpacity={0.7}>
-        <Text style={styles.buttonText}>
-          {'>'}
-        </Text>
+        <Feather name="chevron-right" style={styles.buttonIcon}/>
       </TouchableOpacity>
-    </SafeAreaView>
+    </View>
   )
 }
 
@@ -38,17 +41,21 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'space-around'
+    justifyContent: 'space-around',
+    paddingHorizontal: 20
   },
   title: {
-    fontSize: 32,
+    fontSize: 28,
+    fontFamily: fonts.heading,
     fontWeight: 'bold',
     textAlign: 'center',
     color: colors.heading,
-    marginTop: 38
+    marginTop: 38,
+    lineHeight: 34
   },
   subtitle: {
     textAlign: 'center',
+    fontFamily: fonts.text,
     fontSize: 18,
     paddingHorizontal: 20,
     color: colors.heading
@@ -65,8 +72,8 @@ const styles = StyleSheet.create({
     height: 56,
     width: 56
   },
-  buttonText: {
-    color: colors.white,
-    fontSize: 24
+  buttonIcon: {
+    fontSize: 24,
+    color: colors.white
   }
 })
