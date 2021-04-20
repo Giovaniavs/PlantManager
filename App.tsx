b0895a7fbd6c9b3d948199c7b11ea1e2dc51e86f
Giovaniavs <gavs2@cin.ufpe.br>
2021-04-20 11:09:21 -0300

    Feature(screen): added a confirmation screen in the app

--- App.tsx
+++ App.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import AppLoading from 'expo-app-loading';
+
 import { Welcome } from './src/pages/Welcome';
 import { UserIdentification } from './src/pages/UserIdentification';
+import { Confirmation } from './src/pages/Confirmation';
+
 import {
   useFonts,
   Jost_400Regular,
@@ -19,6 +22,6 @@ export default function App() {
   }
 
   return (
-    <UserIdentification />
+    <Confirmation />
   )
 }
