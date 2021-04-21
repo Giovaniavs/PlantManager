467453671f9bc249b0f934cd0d34d5f13bd22083
Giovaniavs <gavs2@cin.ufpe.br>
2021-04-21 11:04:52 -0300

    Feature(filter-plants): created a function responsible for filter the kind of the plants in each section

--- src/pages/PlantSelect.tsx
+++ src/pages/PlantSelect.tsx
@@ -34,10 +34,26 @@ interface PlantProps {
 export function PlantSelect() {
   const [environments, setEnvironments] = useState<EnvironmentProps[]>([]);
   const [plants, setPlants] = useState<PlantProps[]>([]);
+  const [filteredPlants, setFilteredPlants] = useState<PlantProps[]>([]);
+  const [environmentSelected, setEnvironmentSelected] = useState('all');
+
+  function handleEnvironmentSelected(environment: string) {
+    setEnvironmentSelected(environment)
+
+    if (environment == 'all') {
+      return setFilteredPlants(plants)
+    }
+
+    const filtered = plants.filter(plant =>
+        plant.environments.includes(environment)
+    );
+
+    setFilteredPlants(filtered);
+  }
   
   useEffect(() => {
     async function fetchEnvironment() {
-      const { data } = await api.get('plants_environments');
+      const { data } = await api.get('plants_environments?_sort=title&_order=asc');
       setEnvironments([
         {
           key: 'all',
@@ -51,7 +67,7 @@ export function PlantSelect() {
 
   useEffect(() => {
     async function fetchPlants() {
-    const { data } = await api.get('plants');
+    const { data } = await api.get('plants?_sort=name&_order=asc');
     setPlants(data);
   }
   fetchPlants();
@@ -74,7 +90,11 @@ export function PlantSelect() {
         <FlatList
           data={environments}
           renderItem={({ item }) => (
-            <EnviromentButton title={item.title} />
+            <EnviromentButton
+              title={item.title}
+              active={item.key === environmentSelected}
+              onPress={() => handleEnvironmentSelected(item.key)}
+            />
           )}
           horizontal
           showsHorizontalScrollIndicator={false}
@@ -84,7 +104,7 @@ export function PlantSelect() {
 
       <View style={styles.plants}>
         <FlatList
-          data={plants}
+          data={filteredPlants}
           renderItem={({ item }) => (
             <PlantCardPrimary
               data={item}
