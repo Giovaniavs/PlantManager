b3e834882696f80ad17af10f2770d82b3813a7ad
Giovaniavs <gavs2@cin.ufpe.br>
2021-04-22 22:44:23 -0300

    Feature(my-plant): created my plant screen

--- src/pages/UserIdentification.tsx
+++ src/pages/UserIdentification.tsx
@@ -42,8 +42,19 @@ export function UserIdentification() {
     if (!name) {
       return Alert.alert("Me diz como você se chama! 🌱")
     }
-    await AsyncStorage.setItem('@plantmanager:user', name);
-    navigation.navigate('Confirmation');
+
+    try {
+      await AsyncStorage.setItem('@plantmanager:user', name);
+      navigation.navigate('Confirmation', {
+        title: 'Prontinho!',
+        subtitle: 'Agora vamos começar a cuidar das suas plantinhas com muito cuidado!',
+        buttonTitle: 'Começar',
+        icon: 'smile',
+        nextScreen: 'PlantSelect'
+      });
+    } catch {
+      Alert.alert("Não foi possível salvar o seu nome 😢")
+    }
   }
 
   return (
